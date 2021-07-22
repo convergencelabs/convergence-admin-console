b9738a536f6e5d81acb6a59d3280757a2b7e38fc
Michael MacFadden <michael@convergencelabs.com>
2021-07-21 19:47:28 -0500

    Added link to settings when domain is offline or in maintenance
    Fixes convergencelabs/convergence-project#250

--- src/components/domain/Banner/index.tsx
+++ src/components/domain/Banner/index.tsx
@@ -12,18 +12,21 @@ import React, {ReactElement} from "react";
 import {DomainDescriptor} from "../../../models/DomainDescriptor";
 import {DomainAvailability} from "../../../models/DomainAvailability";
 import {DomainStatus} from "../../../models/DomainStatus";
+import {Link} from "react-router-dom";
 import styles from "./styles.module.css";
+import {toDomainRoute} from "../../../utils/domain-url";
 
 export function DomainBanner(props: { domainDescriptor: DomainDescriptor | null }): ReactElement | null {
   const {domainDescriptor} = props;
   if (domainDescriptor) {
+    const link = toDomainRoute(domainDescriptor?.domainId, "settings/dangerous");
     const {status, availability} = domainDescriptor;
     if (status === DomainStatus.ERROR) {
       return (<div className={styles.error}>The Domain is in an Error State</div>);
     } else if (availability === DomainAvailability.OFFLINE) {
-      return (<div className={styles.offline}>The Domain is Offline</div>);
+      return (<div className={styles.offline}>The Domain is Offline. This can be changed in the <Link className={styles.settingsLink} to={link}>Domain Settings</Link></div>);
     } else if (availability === DomainAvailability.MAINTENANCE) {
-      return (<div className={styles.maintenance}>The Domain is in Maintenance Mode</div>);
+      return (<div className={styles.maintenance}>The Domain is in Maintenance Mode. This can be changed in the <Link className={styles.settingsLink} to={link}>Domain Settings</Link></div>);
     } else {
       return null;
     }
